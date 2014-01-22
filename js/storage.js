f83df08b033c68ab230d6dd58c08c28e0e35929a
sam <sam.ternent@gmail.com>
2014-01-22 19:04:35 +0100

    v2 production
    removed cookie fallback to pure storage handler. lets let storage be
    storage and cookies be cookies :)

--- js/storage.js
+++ js/storage.js
@@ -1,167 +1,78 @@
-window.easyStore = (function () {
-
-    var settings = {
-            storage: true,
-            cookie: true,
-            userSettings: {
-                cookieExp: 365,
-                name: '',
-                value: '',
-                cookieFallBack: false
-            }
-        },
-
-        easyStore = {
-            checkStorage: function (name, value) {
-                settings.userSettings.name = name;
-                settings.userSettings.value = value;
-                if (window.Storage) {
+storageHelper = (function () {
+    var settings = { storage: true },
+        lib = {
+            checkStorage: function () {
+                if (typeof (window.Storage) !== 'undefined') {
                     try {
-                        settings.storage = true;
-
                         // test for safari private browsing exception
                         window.localStorage.setItem('test', 1);
                         window.localStorage.removeItem('test');
-
                         settings.storage = true;
                     } catch (e) {
                         settings.storage = false;
-                    }
-                    try {
-                        settings.cookie = navigator.cookieEnabled ? true : false;
-                        if (typeof navigator.cookieEnabled == 'null' && !settings.cookie)
-                        {
-                            document.cookie = 'test';
-                            settings.cookie = (document.cookie.indexOf('test') != -1) ? true : false;
-                            document.cookie = 'test=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
-                        }
-                    } catch (e) {
-                        settings.cookie = false;
                     }
                 } else {
                     settings.storage = false;
                 }
             },
-
-            // set to storage
-
             // set to local storage
-            setLocal: function (name, value, usrSet) {
-                // set what type has been used
-                settings.userSettings.type = 'setLocal';
-                settings.userSettings.method = 'set';
-                settings.userSettings.cookieFallBack = usrSet;
-
-                easyStore.checkStorage(name, value);
+            setLocal: function (name, value) {
+                lib.checkStorage();
                 if (settings.storage) {
                     window.localStorage.setItem(name, JSON.stringify(value));
-                    return easyStore.exit(true);
+                    return true;
                 } else {
-                    return easyStore.exit(false);
+                    return false;
                 }
             },
             // set to session storage
-            setSession: function (name, value, usrSet) {
-                // set what type has been used
-                settings.userSettings.type = 'setSession';
-                settings.userSettings.method = 'set';
-                settings.userSettings.cookieFallBack = usrSet;
-
-                easyStore.checkStorage(name, value);
+            setSession: function (name, value) {
+                lib.checkStorage();
                 if (settings.storage) {
                     window.sessionStorage.setItem(name, JSON.stringify(value));
-                    return easyStore.exit(true);
+                    return true;
                 } else {
-                    return easyStore.exit(false);
+                    return false;
                 }
             },
-            // set to cookie
-            setCookie: function (name, value) {
-                // set what type has been used
-                settings.userSettings.type = 'setCookie';
-                settings.userSettings.method = 'set';
-
-                easyStore.checkStorage(name, value);
-
-                var d = new Date(),
-                    expires;
-                d.setTime(d.getTime()+(settings.userSettings.cookieExp*24*60*60*1000));
-                expires = 'expires='+d.toGMTString();
-
-                document.cookie = name + '=' + JSON.stringify(value) + '; ' + expires;
-                return easyStore.exit(true);
-            },
-
-            // retrieval from storage
-
             // get from local storage
-            getLocal: function (name, usrSet) {
-                // set what type has been used
-                settings.userSettings.type = 'getLocal';
-                settings.userSettings.method = 'get';
-                settings.userSettings.cookieFallBack = usrSet;
-
-                easyStore.checkStorage(name);
-                if (settings.storage) {
-                    return easyStore.exit(JSON.parse(window.localStorage.getItem(name)));
-                } else {
-                    return easyStore.exit(false);
-                }
+            getLocal: function (name) {
+                lib.checkStorage();
+                return (settings.storage) ? JSON.parse(window.localStorage.getItem(name)) : false;
             },
             // get from session storage
-            getSession: function (name, usrSet) {
-                // set what type has been used
-                settings.userSettings.type = 'getSession';
-                settings.userSettings.method = 'get';
-                settings.userSettings.cookieFallBack = usrSet;
-
-                easyStore.checkStorage(name);
+            getSession: function (name) {
+                lib.checkStorage();
+                return (settings.storage) ? JSON.parse(window.sessionStorage.getItem(name)) : false;
+            },
+            // remove from local storage
+            removeLocal: function (name) {
+                lib.checkStorage();
                 if (settings.storage) {
-                    return easyStore.exit(JSON.parse(window.sessionStorage.getItem(name)));
+                    window.localStorage.removeItem(name)
+                    return true;
                 } else {
-                    return easyStore.exit(false);
+                    return false;
                 }
             },
-            // get from cookie
-            getCookie: function (name, usrSet) {
-                // set what type has been used
-                settings.userSettings.type = 'getCookie';
-                settings.userSettings.method = 'get';
-                settings.userSettings.cookieFallBack = usrSet;
-
-                easyStore.checkStorage(name);
-
-                name += '=';
-                var cookies = document.cookie.split(';');
-                for(var i=0; i<cookies.length; i++) {
-                  var c = cookies[i].trim();
-                  if (c.indexOf(name) == 0) {
-                      return c.substring(name.length,c.length);
-                  }
+            // remove from session storage
+            removeSession: function (name) {
+                lib.checkStorage();
+                if (settings.storage) {
+                    window.sessionStorage.removeItem(name)
+                    return true;
+                } else {
+                    return false;
                 }
-                return false;
-            },
-
-            exit: function (success) {
-                if(!success) {
-                    if (settings.userSettings.cookieFallBack && settings.userSettings.type !== 'setCookie') {
-                        if (settings.userSettings.method === 'set') {
-                            easyStore.setCookie(settings.userSettings.name, settings.userSettings.value);
-                        } else {
-                            easyStore.getCookie(settings.userSettings.name);
-                        }
-                    }
-                }
-                return success;
             }
-    }
+        };
 
     return {
-        setLocal: easyStore.setLocal,
-        setSession: easyStore.setSession,
-        setCookie: easyStore.setCookie,
-        getLocal: easyStore.getLocal,
-        getSession: easyStore.getSession,
-        getCookie: easyStore.getCookie
-    }
+        setLocal: lib.setLocal,
+        setSession: lib.setSession,
+        getLocal: lib.getLocal,
+        getSession: lib.getSession,
+        removeLocal: lib.removeLocal,
+        removeSession: lib.removeSession
+    };
 }());
